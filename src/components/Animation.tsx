cecdfbf4b294b4a76603e87e612a1503f47db6ff
Rigwarl <Avips@mail.ru>
2019-05-17 03:24:31 +0300

    add SSAnimation Component

---
+++ src/components/Animation.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import Spritesheet from 'react-responsive-spritesheet';
+
+type TAnimationData = {
+  from: number;
+  to: number;
+};
+
+type TProps<T> = {
+  image: string;
+  data: THashMap<TAnimationData>;
+  running: T;
+};
+
+const Animation = <T extends string>({ image, data, running }: TProps<T>) => {
+  const totalFrames = Object.values(data).sort((a, b) => b.to - a.to)[0].to;
+
+  return (
+    <Spritesheet
+      image={image}
+      steps={totalFrames}
+      fps={12}
+      direction="forward"
+      loop={true}
+      widthFrame={900}
+      heightFrame={900}
+      onEachFrame={(spritesheet: any) => {
+        const frame: number = spritesheet.getInfo('frame');
+        const { from, to } = data[running];
+
+        console.log(frame, running);
+
+        if (frame < from || frame >= to) {
+          spritesheet.goToAndPlay(from);
+        }
+      }}
+    />
+  );
+};
+
+export default Animation;
--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,22 +1,25 @@
 import React, { useState } from 'react';
-import Spritesheet from 'react-responsive-spritesheet';
-import runSprite from './hero-run.png';
-import attackSprite from './hero-attack.png';
+import Animation from '../Animation';
+// import runSprite from './hero-run.png';
+// import attackSprite from './hero-attack.png';
+import heroSprite from './hero-sprite.png';
+
+const data = {
+  run: { from: 1, to: 12 },
+  attack: { from: 13, to: 24 },
+};
+
+type TAnimations = keyof typeof data;
 
 const Hero = () => {
-  const [sprite, setSprite] = useState(runSprite);
+  const [animation, setAnimation] = useState<TAnimations>('run');
 
   return (
-    <Spritesheet
-      onClick={() => setSprite(attackSprite)}
-      image={sprite}
-      steps={12}
-      fps={12}
-      direction="forward"
-      loop={true}
-      widthFrame={900}
-      heightFrame={900}
-      onLoopComplete={() => setSprite(runSprite)}
+    <Animation<TAnimations>
+      // onClick={() => setAnimation('attack')}
+      image={heroSprite}
+      running={animation}
+      data={data}
     />
   );
 };
