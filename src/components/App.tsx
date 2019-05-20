67734510d9d446faa1042b51c5b7a1aee81613af
Rigwarl <Avips@mail.ru>
2019-05-20 23:40:39 +0300

    add HealthBar

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,14 +1,23 @@
 import React, { useState } from 'react';
 import Hero, { THeroState } from './Hero';
 import Enemy from './Enemy';
+import HealthBar from './HealthBar';
 
 const App = () => {
   const [state, setState] = useState<THeroState>('run');
 
   return (
     <>
-      <Hero state={state} />
-      <Enemy />
+      <div>
+        <HealthBar max={10} current={10} />
+        <HealthBar max={10} current={5} />
+      </div>
+
+      <div>
+        <Hero state={state} />
+        <Enemy />
+      </div>
+
       <button onClick={() => setState(state === 'run' ? 'attack' : 'run')}>
         {state === 'run' ? 'attack' : 'run'}
       </button>
