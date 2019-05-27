97466635c4d5025bd6024baea5f377b3dfc63c9e
Rigwarl <Avips@mail.ru>
2019-05-28 02:23:35 +0300

    add create store function

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
-import store from './store';
+import createStore from './store';
 import App from './components/App';
 
 import './index.css';
 
+const store = createStore();
 const root = document.getElementById('root');
 
 render(
