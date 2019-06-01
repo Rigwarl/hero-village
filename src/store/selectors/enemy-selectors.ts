2763f888abbe0adb250824ddc34c400e4ff82301
Rigwarl <Avips@mail.ru>
2019-06-01 17:16:45 +0300

    change enemy max health formula

--- src/store/selectors/enemy-selectors.ts
+++ src/store/selectors/enemy-selectors.ts
@@ -1,10 +1,10 @@
 import { TState } from 'typesafe-actions';
 
-const ENEMY = { HEALTH: 5, MOVE_DURATION: 1500 };
+const ENEMY = { HEALTH: 5, LEVEL_MULTIPLYER: 0.2, MOVE_DURATION: 1500 };
 
 export const getWave = (state: TState) => state.enemy.wave;
 export const getMaxHealth = (state: TState) =>
-  ENEMY.HEALTH * (1 + 0.2 * state.enemy.wave);
+  ENEMY.HEALTH * (1 + ENEMY.LEVEL_MULTIPLYER * (state.enemy.wave - 1));
 export const getHealth = (state: TState) =>
   Math.max(0, getMaxHealth(state) - state.enemy.missedHealth);
 export const getMove = (state: TState) => state.enemy.move;
